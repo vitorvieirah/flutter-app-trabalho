c81cb6b8e1a3eb8e7bad1e02d0773718ba24d0b0
Vitor Hugo Vieira de Lima <108897429+vitorvieirah@users.noreply.github.com>
2025-06-08 21:02:14 -0300

    Update network_settings.dart

--- lib/configs/network_settings.dart
+++ lib/configs/network_settings.dart
@@ -1,4 +1,4 @@
 class NetworkSettings {
-  static const String authToken = '31322d00-32f2-4a36-9749-f8133b5661a2';
+  static const String authToken = '80e69f72-196b-49b1-a59d-5d3b26342e93';
   static const String serverEndpoint = 'https://pro-api.coinmarketcap.com';
 }
